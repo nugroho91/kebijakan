a55e8a2be1c2b052c8c6b2cf1cc88739c0642c0d
macbook <macbook@macs-MacBook-Pro-2.local>
2023-08-16 14:31:54 +0700

    ok

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Kebijakan Privasi - PT Pusat Gadai Indonesia</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<div class="fixed-header" >
+  <nav class="navbar">
+      <div class="container">
+      <a class="navbar-brand" href="">
+        <img src="image/logo-dark-1024x318.png" alt="Bootstrap">
+      </a>
+      </div>
+  </nav>
+</div>
+<body>
